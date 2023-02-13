4d7b933e87379dd61d5c8c531583475b79da1553
ObodoakorDavid <obodoakordavid@outlook.com>
2023-02-13 12:19:15 +0100

    final changes

--- src/App.js
+++ src/App.js
@@ -15,15 +15,12 @@ const reducer = (state, action) => {
         loading: false,
         doneFetching: true,
       };
-      break;
     case "REFRESH":
       return {
         ...state,
         loading: true,
         doneFetching: false,
       };
-
-      break;
     default:
       return state;
   }
@@ -36,37 +33,24 @@ function App() {
     loading: true,
   });
 
-  // console.log(state.fetching);
-
   let getData = async (url) => {
     let res = await fetch(url);
     let data1 = await res.json();
-    console.log(data1);
     dispatch({ type: "FETCH", data: data1 });
-    console.log(state.data);
   };
 
   let refreshData = async (url) => {
     dispatch({ type: "REFRESH" });
     let res = await fetch(url);
     let data1 = await res.json();
-    console.log(data1);
-    console.log(state.data);
     dispatch({ type: "FETCH", data: data1 });
   };
 
   useEffect(() => {
-    // setTimeout(() => {
     getData("https://api.adviceslip.com/advice");
-    // }, 2000);
   }, []);
 
-  // console.log(state.doneFetching);
-  // console.log(state.loading);
-  // console.log(state.data.slip);
-  //api.adviceslip.com/
-
-  https: return (
+  return (
     <div className="App">
       <Home state={state} refreshData={refreshData} />
     </div>
